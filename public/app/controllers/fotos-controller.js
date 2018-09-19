1f57c08513dc12c67ad6ffb906bdab375695668d
pedro.miranda <pedro.miranda@terracap.df.gov.br>
2018-09-19 16:32:43 -0300

    atualização na controller responsável por listar as imagens cadastradas no banco local angularjs.db

--- public/app/controllers/fotos-controller.js
+++ public/app/controllers/fotos-controller.js
@@ -1,1 +1,11 @@
 /* comentários */
+angular.module('alurapic').controller('FotosController', function ($scope, $http) {
+    /* comentários */
+    $scope.fotos = []
+    var promise = $scope.fotos = $http.get('v1/fotos');
+    promise.then(function (retorno) {
+       $scope.fotos = retorno.data;
+    }).catch(function (error) {
+       console.log(error);
+    });
+});
