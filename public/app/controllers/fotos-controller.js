5c4deed7fe22f50b0fb48835d6cbbc2cddf4b106
pedro.miranda <pedro.miranda@terracap.df.gov.br>
2018-09-21 16:29:17 -0300

    atualização na forma de declara o consumo da api de banco de fotos.

--- public/app/controllers/fotos-controller.js
+++ public/app/controllers/fotos-controller.js
@@ -1,11 +1,24 @@
 /* comentários */
 angular.module('alurapic').controller('FotosController', function ($scope, $http) {
     /* comentários */
-    $scope.fotos = []
-    var promise = $scope.fotos = $http.get('v1/fotos');
-    promise.then(function (retorno) {
-       $scope.fotos = retorno.data;
-    }).catch(function (error) {
-       console.log(error);
+    $scope.fotos = [];
+    $http.get('v1/fotos')
+        .success(function(msg) {
+            console.log(msg);
+            $scope.fotos = msg;
+        }).error(function (msg) {
+        console.log(msg);
     });
 });
+
+
+
+/*
+$scope.fotos = []
+var promise = $scope.fotos = $http.get('v1/fotos');
+promise.then(function (retorno) {
+   $scope.fotos = retorno.data;
+}).catch(function (error) {
+   console.log(error);
+});
+*/
