0c5630b02ec1ae0f73fc0b22122047baa2b5c147
phmiranda <pehhagah.1607@gmail.com>
2019-05-27 21:55:22 -0300

    [update] atualização dos comportamentos estruturais no projeto

--- public/app/controllers/fotos-controller.js
+++ public/app/controllers/fotos-controller.js
@@ -1,21 +1,25 @@
 /* comentários */
+angular.module('alurapic').controller('FotosController', function ($scope, $http) {
+   //
+   $scope.fotos = [];
+   $http.get('v1/fotos').success(function(msg){
+      $scope.fotos = msg;
+      console.log(msg);
+   }).error(function (msg) {
+      console.log(msg);
+   });
 
-//angular.module('alurapic').controller('FotosController', function ($scope, $http) {
-//    $scope.fotos = [];
-//    $http.get('v1/fotos')
-//        .success(function(msg) {
-//            console.log(msg);
-//            $scope.fotos = msg;
-//        }).error(function (msg) {
-//        console.log(msg);
-//    });
-//});
-
-
-$scope.fotos = []
-var promise = $scope.fotos = $http.get('v1/fotos');
-promise.then(function (retorno) {
-   $scope.fotos = retorno.data;
-}).catch(function (error) {
-   console.log(error);
+   /* busca a API usando promisses */
+   /**
+   $scope.fotos = [];
+   var promise = $scope.fotos = $http.get('v1/fotos');
+   promise.then(function (retorno) {
+      $scope.fotos = retorno.data;
+   }).catch(function (error) {
+      console.log(error);
+   });
+   */
 });
+
+
+
